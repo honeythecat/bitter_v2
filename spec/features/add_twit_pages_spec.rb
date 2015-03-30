6deb5f99e11f28e04015fcbe0861d78e1585e63a
Heather <heather@thisisnotanorg.org>
2015-03-30 14:54:51 -0700

    Fix initial spec format.

--- spec/features/add_twit_pages_spec.rb
+++ spec/features/add_twit_pages_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
-describe "Log in and create a twit" do
+describe "the add a twit process" do
   it "creates a new twit" do
     user = FactoryGirl.create(:user)
     visit "/"
+    click_on 'Login'
+    fill_in 'Email', :with => "dog@world.com"
+    fill_in 'Password', :with => "12345678"
+    click_on 'Log in'
     click_on 'WRITE YOUR OWN TWIT!!'
-    twit = FactoryGirl.create(:twit)
-    expect(page).to have_content :twit
+    fill_in 'Twit', :with => 'test twit'
+    click_on 'Create Twit'
+    expect(page).to have_content 'test twit'
+  end
+
+  it "gives error when no title is entered" do
+    user = FactoryGirl.create(:user)
+    visit "/"
+    click_on 'Login'
+    fill_in 'Email', :with => "dog@world.com"
+    fill_in 'Password', :with => "12345678"
+    click_on 'Log in'
+    click_on 'WRITE YOUR OWN TWIT!!'
+    fill_in 'Twit', :with => ''
+    click_on 'Create Twit'
+    expect(page).to have_content 'ERRORS!!'
   end
 end
